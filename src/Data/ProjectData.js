2b36db0b9418ce06e35e00edd7e91a66f1e8e0b6
Kevin Craft <Craft.Kevin.J@Gmail.com>
2020-05-29 16:27:34 -0400

    added Holdem Calculator project

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,20 +1,28 @@
 const projectData = [
-	{
-		name: "Pokedex",
-		description:
-			"Catch'em all with the help of my Pokedex.  Find the stats on all of your favorite Pokemon.  This site uses React and React Router",
-		gitLink: "https://github.com/KevinJCraft/Code-Lou-Pokedex",
-		demoLink: "http://kjc-pokedex.surge.sh/",
-		imgPath: require("../Images/Pokedex-demo-img.JPG")
-	},
-	{
-		name: "Code Lou Landing Page",
-		description:
-			"This is a mock landing page for Code Louisville.  This site uses CSS grid and CSS flexbox as well as Javascript",
-		gitLink: "https://github.com/KevinJCraft/Code-Lou-Landing-Page",
-		demoLink: "http://kjc-code-lou-landing-page.surge.sh/",
-		imgPath: require("../Images/Code-Lou-Landing-Page-img.JPG")
-	}
+  {
+    name: "Holdem Odds Calculator",
+    description:
+      "Find out your odds of winning with every card dealt.  This app was made with create-react-app, react-bootsrap, and bootstrap v4",
+    gitLink: "https://github.com/KevinJCraft/react-holdem-calculator",
+    demoLink: "http://React-Holdem-Calculator.surge.sh",
+    imgPath: require("../Images/React-Holdem-App.JPG"),
+  },
+  {
+    name: "Pokedex",
+    description:
+      "Catch'em all with the help of my Pokedex.  Find the stats on all of your favorite Pokemon.  This site uses React and React Router",
+    gitLink: "https://github.com/KevinJCraft/Code-Lou-Pokedex",
+    demoLink: "http://kjc-pokedex.surge.sh/",
+    imgPath: require("../Images/Pokedex-demo-img.JPG"),
+  },
+  {
+    name: "Code Lou Landing Page",
+    description:
+      "This is a mock landing page for Code Louisville.  This site uses CSS grid and CSS flexbox as well as Javascript",
+    gitLink: "https://github.com/KevinJCraft/Code-Lou-Landing-Page",
+    demoLink: "http://kjc-code-lou-landing-page.surge.sh/",
+    imgPath: require("../Images/Code-Lou-Landing-Page-img.JPG"),
+  },
 ];
 
 export default projectData;
