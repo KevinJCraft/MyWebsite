2069955d073383d93ec25c9e3002b9b806b9a73d
Kevin Craft <Craft.Kevin.J@Gmail.com>
2020-09-15 13:13:56 -0400

    added whisper-it

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,5 +1,13 @@
 const projectData = [
   {
+    name: "Whisper-It",
+    description:
+      "This is a full stack MERN application that uses Redux for state management.  It also uses Material-UI and is hosted by Heroku.",
+    gitLink: "https://github.com/KevinJCraft/whisper-it",
+    demoLink: "https://morning-chamber-67463.herokuapp.com/",
+    imgPath: require("../Images/whisper-it.JPG"),
+  },
+  {
     name: "Chat-App",
     description:
       "This is a Node.js application with an Express server and React front-end.  This also uses Socket.io for real time communication.",
@@ -10,7 +18,7 @@ const projectData = [
   {
     name: "Poker Vloger Website",
     description:
-      "This website was made with Create-React-App, React Router, bootstrap, and Youtube's Data API.  This site is hosted by Netlify",
+      "This website was made with Create-React-App, React Router, bootstrap, and Youtube's Data API.  This site is hosted by Netlify.",
     gitLink: "https://github.com/KevinJCraft/adamrudepoker",
     demoLink: "https://AdamRudePoker.com",
     imgPath: require("../Images/Adam-Rude-Poker.JPG"),
@@ -18,7 +26,7 @@ const projectData = [
   {
     name: "Holdem Odds Calculator",
     description:
-      "Find out your odds of winning with every card dealt.  This app was made with create-react-app, react-bootsrap, and bootstrap v4",
+      "Find out your odds of winning with every card dealt.  This app was made with create-react-app, react-bootsrap, and bootstrap v4.",
     gitLink: "https://github.com/KevinJCraft/react-holdem-calculator",
     demoLink: "http://React-Holdem-Calculator.surge.sh",
     imgPath: require("../Images/React-Holdem-App.JPG"),
@@ -26,7 +34,7 @@ const projectData = [
   {
     name: "Pokedex",
     description:
-      "Catch'em all with the help of my Pokedex.  Find the stats on all of your favorite Pokemon.  This site uses React, React Router and PokeApi",
+      "Catch'em all with the help of my Pokedex.  Find the stats on all of your favorite Pokemon.  This site uses React, React Router and PokeApi.",
     gitLink: "https://github.com/KevinJCraft/Code-Lou-Pokedex",
     demoLink: "http://kjc-pokedex.surge.sh/",
     imgPath: require("../Images/Pokedex-demo-img.JPG"),
@@ -34,7 +42,7 @@ const projectData = [
   {
     name: "Code Lou Landing Page",
     description:
-      "This is a mock landing page for Code Louisville.  This site uses CSS grid and CSS flexbox as well as Javascript",
+      "This is a mock landing page for Code Louisville.  This site uses CSS grid and CSS flexbox as well as Javascript.",
     gitLink: "https://github.com/KevinJCraft/Code-Lou-Landing-Page",
     demoLink: "http://kjc-code-lou-landing-page.surge.sh/",
     imgPath: require("../Images/Code-Lou-Landing-Page-img.JPG"),
